a26d7791d8ff5f8a136979d0f6f4bfb6d28729d9
LoKias_ <diseursmat@gmail.com>
2025-03-18 22:36:13 +0100

    Versions 1.0 - Detection of a ennemie

---
+++ ennemiIA.lua
@@ -0,0 +1,67 @@
+-- Versions 1.0
+local sneakDetectionArea = workspace.IADUMMY:WaitForChild("sneakDetection")
+local isPlayerDetected = false
+
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+
+local npc = workspace:WaitForChild("IADUMMY")
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+
+local function isPlayerBehindWall(npcHead, playerHead)
+    local ray = Ray.new(npcHead.Position, (playerHead.Position - npcHead.Position).Unit * (playerHead.Position - npcHead.Position).Magnitude)
+
+    local ignoreList = {npc, character}
+
+    local hit = workspace:FindPartOnRayWithIgnoreList(ray, ignoreList)
+
+    if hit then
+        return true
+    else
+        return false
+    end
+end
+
+sneakDetectionArea.Touched:Connect(function(hit)
+    if isPlayerDetected == false then
+        if hit.Parent == character then
+            print("Player touched the sneakDetection Area")
+            isPlayerDetected = true
+        end
+    end
+end)
+
+sneakDetectionArea.TouchEnded:Connect(function(hit)
+    if hit.Parent == character then
+        print("Player isn't touching the sneakDetection area anymore")
+        isPlayerDetected = false
+    end
+end)
+
+RunService.RenderStepped:Connect(function()
+    if character and character:FindFirstChild("Head") and npc and npc:FindFirstChild("Head") then
+        if isPlayerDetected == false then
+            local npcToCharacter = (character.Head.Position - npc.Head.Position).Unit
+            local differenceBetweenPosition = character.Head.Position.Z - npc.Head.Position.Z
+
+            local cframe = CFrame.new(npc.Head.Position, npc.Head.Position + npcToCharacter)
+            local npcLook = npc.Head.CFrame.LookVector
+
+            local maxview = 0.7001 -- 45° View in front of the NPC
+
+            local dotProduct = npcToCharacter:Dot(npcLook)
+
+            if dotProduct > maxview then
+                -- Check if the player is behind a wall
+                if not isPlayerBehindWall(npc.Head, character.Head) then
+                    print("Player Detected by the view of the NPC")
+                else
+                    print("Player is behind a wall and cannot be seen")
+                end
+            else
+                print("Player isn't detected")
+            end
+        end
+    end
+end)
